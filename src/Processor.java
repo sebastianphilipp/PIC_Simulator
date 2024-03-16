8da54c52db7f078069ffcc800697359cebeec1cb
suko3003 <124807137+suko3003@users.noreply.github.com>
2024-03-16 16:21:25 +0100

    updated decode for literals

--- src/Processor.java
+++ src/Processor.java
@@ -14,8 +14,7 @@ public class Processor {
 
     public int[] decode() {
         int command = rom.readAddress(ram.getProgramCounter());
-        System.out.println(Integer.toBinaryString(rom.readAddress(0)));
-        System.out.println(Integer.toBinaryString(command));
+        //decode for literal & control commands
         switch (command >> 12) {
             case 0b11:
                 switch ((command >> 8) & 0b001111) {
@@ -41,11 +40,36 @@ public class Processor {
                         System.out.println("xorlw");
                         break;
                     default:
-                        System.out.println("No Command");
+                        System.out.println("No for literal 11 Command");
                         break;
                 }
                 break;
-
+            case 0b10:
+                if (((command >> 8) & 0b001111) == 0b1000) {
+                    System.out.println("goto");
+                } else {
+                    System.out.println("call");
+                }
+                break;
+            case 0b00:
+                switch (command & 0b1111) {
+                    case 0b0100:
+                        System.out.println("CLRWDT");
+                        break;
+                    case 0b1001:
+                        System.out.println("RETFIE");
+                        break;
+                    case 0b1000:
+                        System.out.println("RETURN");
+                        break;
+                    case 0b0011:
+                        System.out.println("SLEEP");
+                        break;
+                    default:
+                        System.out.println("No for literal 00 Command");
+                        break;
+                }
+                break;
             default:
                 System.out.println("default");
                 break;
